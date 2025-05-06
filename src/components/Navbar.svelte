1e38c12f6bc1b236788d00074c9b4570feb345bc
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2025-05-06 04:36:43 -0400

    navbar work

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,45 @@
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl" aria-label="home" href="#home">Jaedon Smith</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li>
+				<a href="#about">About Me</a>
+			</li>
+			<li>
+				<a href="#projects">Projects</a>
+			</li>
+			<li>
+				<a href="#skills">Skills</a>
+			</li>
+			<li>
+				<a href="#contacts">Contact</a>
+			</li>
+			<li>
+				<details>
+					<summary>Parent</summary>
+					<ul class="bg-base-100 rounded-t-none p-2">
+						<li><a>Link 1</a></li>
+						<li><a>Link 2</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+		<button class="btn btn-square btn-ghost md:hidden" aria-label="dropdown">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block h-5 w-5 stroke-current"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+				></path>
+			</svg>
+		</button>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Navbar />
+<main>
+	{@render children()}
+</main>
